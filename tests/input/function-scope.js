989be27f75684497be6be4ca07bee15b27541c63
Stepan Vanzuriak <stepanvanzuriak@gmail.com>
2020-05-04 09:11:54 +0300

    Add extra deep test

--- tests/input/function-scope.js
+++ tests/input/function-scope.js
@@ -6,4 +6,16 @@ function scope() {
   let a = 'str';
 
   a = 'str';
+
+  function deep() {
+    let a = 12;
+
+    a = 67;
+
+    function deeper() {
+      let c = true;
+
+      c = false;
+    }
+  }
 }
